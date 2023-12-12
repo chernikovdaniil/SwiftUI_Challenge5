8e25583f8977beaadc8a8c5b3b4aeaac6117bed9
Danil Chernikov <danil.chernikov@onix-systems.com>
2023-12-12 11:00:20 +0100

    Initial Commit

---
+++ SwiftUI_Challenge5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Challenge5
+//
+//  Created by Danil Chernikov on 12.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Challenge5/SwiftUI_Challenge5App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Challenge5App.swift
+//  SwiftUI_Challenge5
+//
+//  Created by Danil Chernikov on 12.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Challenge5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
